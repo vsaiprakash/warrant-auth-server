c0a439913bd6976377ec72ecc8806dfc947d5c9f
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-15 15:26:40 +0000

    Initial Commit;

---
+++ src/main/java/com/vsaiprakash/warrentauthserver/WarrentAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.vsaiprakash.warrentauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrentAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarrentAuthServerApplication.class, args);
+	}
+
+}
