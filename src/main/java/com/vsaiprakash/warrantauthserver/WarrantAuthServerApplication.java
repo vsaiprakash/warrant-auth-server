a43f7d8a8b060e79043e8ebf6964f72862a1e971
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-16 04:25:30 +0000

    enabled auth server for spring boot;

--- src/main/java/com/vsaiprakash/warrantauthserver/WarrantAuthServerApplication.java
+++ src/main/java/com/vsaiprakash/warrantauthserver/WarrantAuthServerApplication.java
@@ -2,7 +2,9 @@ package com.vsaiprakash.warrantauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableAuthorizationServer
 @SpringBootApplication
 public class WarrantAuthServerApplication {
 
