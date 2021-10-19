da37c720939389ea1dc8a85bc0b5108105362f5e
V. Sai Prakash <vukkumsp@gmail.com>
2021-10-19 06:24:55 +0000

    updated code;

--- src/test/java/com/vsaiprakash/warrantauthserver/WarrantAuthServerApplicationTests.java
+++ src/test/java/com/vsaiprakash/warrantauthserver/WarrantAuthServerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.vsaiprakash.warrantauthserver;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WarrantAuthServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
